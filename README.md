89eaf167d40412511c963e3a8567dfb9a531ec5f
Ubaidmalik9567 <Ubaidmalik9567@yahoo.com>
2025-01-08 17:08:46 +0500

    initialize project setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# spotify-hybrid-recommender-system
+==============================
+
+A weighted Hybrid Recommender System for Spotify music recommendations.
+
+> **Note**: Currently in the **development phase**.
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A weighted Hybrid Recommend-er System for spotify music recommendations',
+    author='Ubaid ur Rehman',
+    license='MIT',
+)
